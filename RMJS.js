2cf8bb46f2634a2c17bee53ed816a761f292f49e
Kiran Bhadury <kbhadury@ucdavis.edu>
2016-09-28 18:52:41 -0700

    Test

--- RMJS.js
+++ RMJS.js
@@ -418,7 +418,7 @@ function getUtilIcon(util){
 	if(util === "HOA" || util === "HOME OWNERS ASSOCIATION"){
 		return $("<span class='glyphicon glyphicon-home' style='color:#4d94ff'></span>");
 	}
-	if(util.includes("COSTCO")){
+	if(util === "COSTCO"){
 	   	return $("<span class='glyphicon glyphicon-credit-card' style='color:#ff4d4d'></span>");
 	}
 
