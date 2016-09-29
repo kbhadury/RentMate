14956191eb4b01c0052a6e9cc3231966077ae38f
Kiran Bhadury <kbhadury@ucdavis.edu>
2016-09-28 18:48:09 -0700

    Added an icon

--- RMJS.js
+++ RMJS.js
@@ -418,6 +418,9 @@ function getUtilIcon(util){
 	if(util === "HOA" || util === "HOME OWNERS ASSOCIATION"){
 		return $("<span class='glyphicon glyphicon-home' style='color:#4d94ff'></span>");
 	}
+	if(util.includes("COSTCO"){
+	   return $("<span class='glyphicon glyphicon-credit-card' style='color:#ff4d4d'></span>");
+	}
 
 	//Default
 	return $("<span class='glyphicon glyphicon-ok-circle' style='color:#d9d9d9'></span>");
